254380bbced6ce3ce6e8b14b34b241b9aa4097e5
Anuj Verma <anujverma3563@gmail.com>
2023-10-19 19:02:00 +0530

    Setup basic Application and Entry Point

---
+++ Hazel/src/Hazle.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//Use by Applications 
+
+#include<stdio.h>
+#include"Hazel\Application.h"
+
+//--------EntryPoint-------------
+#include"Hazel\EntryPoint.h"
+//-------------------------------
